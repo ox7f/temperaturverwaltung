b617ff8a4dfdf6edc2664ee7f11411a09a2954b8
ox7f <77727665+ox7f@users.noreply.github.com>
2021-04-28 17:58:05 +0200

    Update index.js

--- user/routes/index.js
+++ user/routes/index.js
@@ -2,7 +2,7 @@ const path              = require('path');
 const express           = require('express');
 const router            = express.Router();
 
-// beim aufruf von http://localhost:$PORT/ wird die datei views/index.html angezeigt
+// beim aufruf von http://localhost:$PORT/ wird die datei views/index.html zum client geschickt
 router.get('/', (req, res) => {
     return res.sendFile(path.resolve('views/index.html'));
 });
@@ -10,10 +10,8 @@ router.get('/', (req, res) => {
 // das hier ist die zentrale backend stelle
 // hier koennen wir POST Requests vom user/admin interface verarbeiten
 
-// todo - endpoints erstellen
+// todo - Endpoints erstellen
 // hier am besten die npm express docs anschauen (https://www.npmjs.com/package/express),
-// das req object besitzt halt die daten, die wir vom admin-interface bzw vom user-interface schicken
-// das res object besitzt funktionen wie res.send() / res.sendFile(), damit senden wir eine response zurueck (update war erfolgreich beispielsweise)
 
 router.post('/api/todo', (req, res) => {
 	// todo
