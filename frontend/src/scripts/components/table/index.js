78412daa5b654cc85e20eb7041f2cc68a2979cdb
ox7f <ox7f@protonmail.com>
2021-06-21 21:37:59 +0200

    .

--- frontend/src/scripts/components/table/index.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-import { TableComponentDefinition } from './table.component';
-
-export default angular
-    .module('table-module', [])
-    .component('table', TableComponentDefinition)
-    .name;
