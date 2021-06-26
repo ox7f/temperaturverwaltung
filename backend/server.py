5c63e6edb56b5193ea71da314b5384f02509efe7
ox7f <ox7f@protonmail.com>
2021-06-26 08:18:03 +0200

    .

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-    
+import mysql.connector
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+# todo: datenbank anbindung
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password=''
+)
+
+print(mydb)
 
 # neuer client
 @socketio.on('connect')
