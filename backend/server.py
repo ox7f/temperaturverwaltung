08df25dc7634d22b2a704a1914585db8a6d689c5
ox7f <ox7f@protonmail.com>
2021-06-26 11:32:37 +0200

    login

--- backend/server.py
+++ backend/server.py
@@ -1,21 +1,81 @@
-from flask import Flask, render_template
+from flask import Flask, session, jsonify, request
+from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO, emit
 import mysql.connector
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
+cors = CORS(app)
+app.config['SECRET_KEY'] = 'WHKAWSF hat ganz dicke Eier'
+app.config['CORS_HEADERS'] = 'Content-Type'
+socketio = SocketIO(app, cors_allowed_origins='http://localhost:8080')
 
-# todo: datenbank anbindung
+# datenbank anbindung
 mydb = mysql.connector.connect(
     host='localhost',
     user='root',
-    password=''
+    password='',
+    database='temperaturverwaltung'
 )
 
-print(mydb)
+# routing stuff
+@app.route('/api/login', methods=['GET', 'POST'])
+@cross_origin()
+def login():
+    data = request.json
+    logincursor = mydb.cursor()
 
-# neuer client
+    sql = 'SELECT `Administrator` FROM benutzer WHERE Anmeldename = %s AND Passwort = %s'
+    val = (data['name'], data['password'])
+
+    logincursor.execute(sql, val)
+    userResult = logincursor.fetchone()
+    logincursor.close()
+
+    if (userResult):
+        session['logged_in'] = True
+        status = userResult[0]
+
+    response = {
+        'status': status,
+        'isAdmin': userResult[0],
+    }
+
+    return jsonify(response)
+
+@app.route('/api/logout', methods=['GET', 'POST'])
+@cross_origin()
+def logout():
+    session.pop('logged_in', None)
+    return jsonify({'message': 'success'})
+
+@app.route('/api/addTemperatur', methods=['GET', 'POST'])
+@cross_origin()
+def addTemperature():
+    print(request)
+
+    #addcursor = mydb.cursor()
+
+    #sql = 'INSERT INTO temperatur (SensorID, Temperatur) VALUES (%s, %s)'
+    #val = (1, 20.5)
+
+    #addcursor.execute(sql, val)
+    #mydb.commit()
+    #addcursor.close()
+
+    emit('temperature-added', 'todo: table data aus der datenbank')
+
+    message = 'todo: success oder error'
+    return message
+
+@app.route('/api/removeTemperature', methods=['GET', 'POST'])
+@cross_origin()
+def removeTemperature():
+    print(request)
+    emit('temperature-removed', 'todo: table data aus der datenbank')
+    message = 'todo: success oder error'
+    return message
+
+# socket stuff
 @socketio.on('connect')
 def connected():
     emit('table-data', 'todo: table data aus der datenbank')
--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -5,15 +5,78 @@ import tableModule from './components/table';
 
 let app = angular.module('app', [tableModule]);
 
-app.service('Socket', ['$rootScope', '$window', ($rootScope, $window) => {
+app.service('SessionService', function() {
+    let Service = this;
+
+    Service.getUser = () => {
+        return User;
+    };
+
+    Service.login = (data) => {  
+        return fetch(`http://${SOCKET_HOST}/api/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log('loginCall', json);
+        });
+    };
+    
+    Service.logout = (data) => {
+        return fetch(`${SOCKET_HOST}/api/logout`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+    };
+});
+
+app.service('SocketService', function() {
     let Service = this,
         socket = io(SOCKET_HOST);
 
-    socket.on('table-data', (data) => {
+    socket
+    .on('table-data', (data) => {
         console.log('table-data', data);
+    })
+    .on('temperature-added', (data) => {
+        console.log('temperature-added', data);
+    })
+    .on('temperature-removed', (data) => {
+        console.log('temperature-removed', data);
     });
 
     return Service;
-}]);
+});
+
+app.service('DataService', function() {
+    let Service = this;
+
+    Service.addTemepratur = (data) => {
+        return fetch(`${SOCKET_HOST}/api/addTemperatur`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+    };
+
+    Service.removeTemepratur = (data) => {
+        return fetch(`${SOCKET_HOST}/api/removeTemperatur`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+    };
+});
 
 export default app;
