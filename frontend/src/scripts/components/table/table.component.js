873173d878a2acc03253db72b09aa663090c442e
ox7f <ox7f@protonmail.com>
2021-07-05 12:00:31 +0200

    filter

--- frontend/src/scripts/components/table/table.component.js
+++ frontend/src/scripts/components/table/table.component.js
@@ -3,10 +3,41 @@ import template from './table.html';
 class TableComponent {
     constructor($scope) {
         // TODO - filter bauen
-        $scope.filterOptions = ['TemperaturID', 'Zeit', 'Temperatur'];
+        $scope.filterOptions = [
+            {
+                name: 'ID aufsteigend',
+                label: 'TemperaturID',
+                option: false
+            },
+            {
+                name: 'ID absteigend',
+                label: 'TemperaturID',
+                option: true
+            },
+            {
+                name: 'Zeit aufsteigend',
+                label: 'Zeit',
+                option: false
+            },
+            {
+                name: 'Zeit absteigend',
+                label: 'Zeit',
+                option: true
+            },
+            {
+                name: 'Temperatur aufsteigend',
+                label: 'Temperatur',
+                option: false
+            },
+            {
+                name: 'Temperatur absteigend',
+                label: 'Temperatur',
+                option: true
+            }
+        ];
 
         $scope.filterChange = () => {
-            $scope.$evalAsync();
+            $scope.$evalAsync
             console.log('new filterSetting:', $scope.selectedFilter);
         };
 
